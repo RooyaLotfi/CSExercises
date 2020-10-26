88785c8a53513344cebb404457e6ca22bb5900d3
Roya Lotfi <lotfir@myumanitoba.ca>
2020-10-26 14:51:36 -0500

    Finished the construtors

--- Person.cs
+++ Person.cs
@@ -6,5 +6,33 @@ namespace ObjectOrientedPrograms
 {
     class Person
     {
+        private string id;
+        private string name = "";
+        private int age = 0;
+        private char gender = 'M';
+        private double weight = 0;
+        private double height = 0;
+
+        public Person(string id){
+            this.id = id;
+        }
+
+        public Person(string id, string name, int age, char gender, double weight){
+            this.id = id;
+            this.name = name;
+            this.age = age;
+            this.gender = gender;
+        }
+
+        public Person(string id, string name, int age, char gender, double weight, double height){
+            this.id = id;
+            this.name = name;
+            this.age = age;
+            this.gender = gender;
+            this.weight = weight;
+            this.height = height;
+        }
+
+
     }
 }
